1f66fa6a5e7ac943f080695d7471b83e41b65ecc
vi <vanvibig@gmail.com>
2020-02-29 01:07:23 +0700

    beginer for call express api

--- functions/client/src/App.js
+++ functions/client/src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("/languages/all")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result);
+                    console.log(result.data);
+                    let newItems = [];
+                    for (const [key, value] of Object.entries(result.data)) {
+                        // console.log(key, value);
+                        newItems.push({
+                            short: key,
+                            name: value.name,
+                            nativeName: value.nativeName
+                        })
+                    }
+                    console.log(newItems);
+                    this.setState({
+                        isLoaded: true,
+                        items: newItems
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const {error, isLoaded, items} = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <ul>
+                    {items.map(item => (
+                        <li key={item.name}>
+                            {item.short} | {item.name} | {item.nativeName}
+                        </li>
+                    ))}
+                </ul>
+            );
+        }
+
+        // return (
+        //     <div className="App">
+        //         <header className="App-header">
+        //             <p>
+        //                 Edit <code>src/App.js</code> and save to reload.
+        //             </p>
+        //             <a
+        //                 className="App-link"
+        //                 href="https://reactjs.org"
+        //                 target="_blank"
+        //                 rel="noopener noreferrer"
+        //             >
+        //                 Learn React
+        //             </a>
+        //         </header>
+        //     </div>
+        // );
+    }
+}
--- functions/link.md
+++ functions/link.md
@@ -1,2 +1,3 @@
 https://itnext.io/running-express-js-on-firebase-cloud-functions-a20b536c6aec
 https://dev.to/loujaybee/using-create-react-app-with-express
+https://reactjs.org/docs/faq-ajax.html
